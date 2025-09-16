3bad60e056ce3f477e5f7b7ae4a2519b48511041
KAKARLA YESWANTH KUMAR <163734820+KYeswanthKumar@users.noreply.github.com>
2025-09-16 12:01:46 +0530

    Update cartService.js

--- src/services/cartService.js
+++ src/services/cartService.js
@@ -1,17 +1,31 @@
 import axios from "axios";
 
-const API_URL = "http://ec2-3-89-63-12.compute-1.amazonaws.com:8081/auth";
+const API_URL = "http://ec2-3-89-63-12.compute-1.amazonaws.com:8081/api/cart";
 
-export const login = async (username, password) => {
-  const response = await axios.post(`${API_URL}/login`, { username, password });
-  localStorage.setItem("token", response.data);
+const API_BASE = "http://ec2-3-89-63-12.compute-1.amazonaws.com:8081/api/cart";
+
+export const addToCart = async (userId, productId) => {
+  return await axios.post(`${API_BASE}/add`, {
+    userId,
+    productId,
+    quantity: 1
+  });
+};
+
+export const getCartItems = async (userId) => {
+  const response = await axios.get(`${API_BASE}/user/${userId}`);
   return response.data;
 };
 
-export const signup = async (username, email, password) => {
-  return axios.post(`${API_URL}/signup`, { username, email, password });
+export const clearCart = async (userId) => {
+  return await axios.delete(`${API_BASE}/clear/${userId}`);
 };
 
-export const logout = () => {
-  localStorage.removeItem("token");
+// Remove Cart Item
+export const removeCartItem = async (cartItemId) => {
+    try {
+        await axios.delete(`${API_URL}/remove/${cartItemId}`);
+    } catch (error) {
+        console.error("Error removing item from cart", error);
+    }
 };
